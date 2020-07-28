16d7874b3fddd2e9ab805382bcb82d67a5206155
Christian Alpoim <chrufes@yahoo.com.br>
2020-07-28 20:41:18 -0300

    Add Slider component

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -6,6 +6,14 @@ export default function CadastroCategoria() {
   return (
     <PageDefault>
       <h1>Cadastro de categoria</h1>
+
+      <form>
+        <label>
+          Nome da Categoria: <input type="text" />
+        </label>
+        <button>Cadastrar</button>
+      </form>
+
       <Link to="/">Ir para home</Link>
     </PageDefault>
   );
