4d3ad6a243cf3562b6c7c268be329911585748e2
Christian Alpoim <chrufes@yahoo.com.br>
2020-07-28 15:54:38 -0300

    Add PageDefault and pages

---
+++ src/pages/cadastro/Categoria/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PageDefault from "../../../components/PageDefault";
+import { Link } from "react-router-dom";
+
+export default function CadastroCategoria() {
+  return (
+    <PageDefault>
+      <h1>Cadastro de categoria</h1>
+      <Link to="/">Ir para home</Link>
+    </PageDefault>
+  );
+}
